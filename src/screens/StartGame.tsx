5678da8f315cc48eb7b7e5fcb6ad4c8ebe419259
Kenny-ai <63028105+Kenny-ai@users.noreply.github.com>
2022-08-29 22:52:52 +0100

    ...

--- src/screens/StartGame.tsx
+++ src/screens/StartGame.tsx
@@ -6,7 +6,7 @@ const StartGame = ({ startBoard }: Props) => {
   return (
     <div className="bg-main-bg h-screen py-4">
       <h1 className="text-3xl font-bold flex justify-center items-center h-[10rem] md:text-4xl">
-        memorial
+        memory
       </h1>
       <div className="flex justify-center items-center">{startBoard}</div>
     </div>
